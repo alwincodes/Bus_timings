b586463365145585665c590f4226db08c06e0e50
alwincodes <alwinmathew4@gmail.com>
2021-11-07 15:10:57 +0530

    all station resolver complete

--- server/src/controller/StationResolver.ts
+++ server/src/controller/StationResolver.ts
@@ -1,14 +1,24 @@
-import { Query, Resolver } from "type-graphql";
+import { FieldResolver, Query, Resolver, Root } from "type-graphql";
 import { getManager } from "typeorm";
+import Bus from "../entities/Bus";
+import BusToStation from "../entities/BusToStation";
 import Station from "../entities/Station";
 
-@Resolver(() => Station)
+@Resolver(() => BusToStation)
 class StationResolver {
+    @FieldResolver(() => Bus, { nullable: true })
+    async bus(@Root() b2s: BusToStation): Promise<Bus | undefined> {
+        const manager = getManager();
+        return await manager.findOne(Bus, { where: `id = ${b2s.busId}` });
+    }
+
     @Query(() => [Station])
     async getAllStation(): Promise<Station[]> {
         const manager = getManager();
-        const stations = await manager.find(Station);
-        console.log(stations);
+        const stations = await manager.find(Station, {
+            relations: ["busToStation"],
+        });
+
         return stations;
     }
 }
